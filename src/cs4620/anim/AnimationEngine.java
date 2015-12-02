d68e037f6f242858f14f9c8b4c3209d3a707c3bf
Andres Gutierrez <aeg72@cornell.edu>
2015-12-02 00:13:24 -0500

    Box.java done, needs testing
    computeBoundingBox() for box has not been tested
    also added AnimationEngine.java from a7

--- src/cs4620/anim/AnimationEngine.java
+++ src/cs4620/anim/AnimationEngine.java
@@ -1,18 +1,21 @@
 package cs4620.anim;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import cs4620.common.Scene;
 import cs4620.common.SceneObject;
 import cs4620.common.event.SceneTransformationEvent;
-import egl.math.Matrix4;
-import egl.math.Vector3;
 import egl.math.Matrix3;
+import egl.math.Matrix4;
 import egl.math.Quat;
+import egl.math.Vector3;
 
 /**
- * A Component Resting Upon Scene That Gives
- * Animation Capabilities
+ * A Component Resting Upon Scene That Gives Animation Capabilities
+ * 
  * @author Cristian
  *
  */
@@ -40,138 +43,226 @@ public class AnimationEngine {
 
 	/**
 	 * An Animation Engine That Works Only On A Certain Scene
-	 * @param s The Working Scene
+	 * 
+	 * @param s
+	 *            The Working Scene
 	 */
 	public AnimationEngine(Scene s) {
 		scene = s;
 	}
-	
+
 	/**
 	 * Set The First And Last Frame Of The Global Timeline
-	 * @param start First Frame
-	 * @param end Last Frame (Must Be Greater Than The First
+	 * 
+	 * @param start
+	 *            First Frame
+	 * @param end
+	 *            Last Frame (Must Be Greater Than The First
 	 */
 	public void setTimelineBounds(int start, int end) {
 		// Make Sure Our End Is Greater Than Our Start
-		if(end < start) {
+		if (end < start) {
 			int buf = end;
 			end = start;
 			start = buf;
 		}
-		
+
 		frameStart = start;
 		frameEnd = end;
 		moveToFrame(curFrame);
 	}
+
 	/**
 	 * Add An Animating Object
-	 * @param oName Object Name
-	 * @param o Object
+	 * 
+	 * @param oName
+	 *            Object Name
+	 * @param o
+	 *            Object
 	 */
 	public void addObject(String oName, SceneObject o) {
 		timelines.put(oName, new AnimTimeline(o));
 	}
+
 	/**
 	 * Remove An Animating Object
-	 * @param oName Object Name
+	 * 
+	 * @param oName
+	 *            Object Name
 	 */
 	public void removeObject(String oName) {
 		timelines.remove(oName);
 	}
 
 	/**
-	 * Set The Frame Pointer To A Desired Frame (Will Be Bounded By The Global Timeline)
-	 * @param f Desired Frame
+	 * Set The Frame Pointer To A Desired Frame (Will Be Bounded By The Global
+	 * Timeline)
+	 * 
+	 * @param f
+	 *            Desired Frame
 	 */
 	public void moveToFrame(int f) {
-		if(f < frameStart) f = frameStart;
-		else if(f > frameEnd) f = frameEnd;
+		if (f < frameStart)
+			f = frameStart;
+		else if (f > frameEnd)
+			f = frameEnd;
 		curFrame = f;
 	}
+
 	/**
 	 * Looping Forwards Play
-	 * @param n Number Of Frames To Move Forwards
+	 * 
+	 * @param n
+	 *            Number Of Frames To Move Forwards
 	 */
 	public void advance(int n) {
 		curFrame += n;
-		if(curFrame > frameEnd) curFrame = frameStart + (curFrame - frameEnd - 1);
+		if (curFrame > frameEnd)
+			curFrame = frameStart + (curFrame - frameEnd - 1);
 	}
+
 	/**
 	 * Looping Backwards Play
-	 * @param n Number Of Frames To Move Backwards
+	 * 
+	 * @param n
+	 *            Number Of Frames To Move Backwards
 	 */
 	public void rewind(int n) {
 		curFrame -= n;
-		if(curFrame < frameStart) curFrame = frameEnd - (frameStart - curFrame - 1);
+		if (curFrame < frameStart)
+			curFrame = frameEnd - (frameStart - curFrame - 1);
 	}
 
 	public int getCurrentFrame() {
 		return curFrame;
 	}
+
 	public int getFirstFrame() {
 		return frameStart;
 	}
+
 	public int getLastFrame() {
 		return frameEnd;
 	}
+
 	public int getNumFrames() {
 		return frameEnd - frameStart + 1;
 	}
 
 	/**
-	 * Adds A Keyframe For An Object At The Current Frame
-	 * Using The Object's Transformation - (CONVENIENCE METHOD)
-	 * @param oName Object Name
+	 * Adds A Keyframe For An Object At The Current Frame Using The Object's
+	 * Transformation - (CONVENIENCE METHOD)
+	 * 
+	 * @param oName
+	 *            Object Name
 	 */
 	public void addKeyframe(String oName) {
 		AnimTimeline tl = timelines.get(oName);
-		if(tl == null) return;
+		if (tl == null)
+			return;
 		tl.addKeyFrame(getCurrentFrame(), tl.object.transformation);
 	}
+
 	/**
-	 * Removes A Keyframe For An Object At The Current Frame
-	 * Using The Object's Transformation - (CONVENIENCE METHOD)
-	 * @param oName Object Name
+	 * Removes A Keyframe For An Object At The Current Frame Using The Object's
+	 * Transformation - (CONVENIENCE METHOD)
+	 * 
+	 * @param oName
+	 *            Object Name
 	 */
 	public void removeKeyframe(String oName) {
 		AnimTimeline tl = timelines.get(oName);
-		if(tl == null) return;
+		if (tl == null)
+			return;
 		tl.removeKeyFrame(getCurrentFrame(), tl.object.transformation);
 	}
-	
+
 	/**
-	 * Loops Through All The Animating Objects And Updates Their Transformations To
-	 * The Current Frame - For Each Updated Transformation, An Event Has To Be 
-	 * Sent Through The Scene Notifying Everyone Of The Change
+	 * Loops Through All The Animating Objects And Updates Their Transformations
+	 * To The Current Frame - For Each Updated Transformation, An Event Has To
+	 * Be Sent Through The Scene Notifying Everyone Of The Change
 	 */
 
 	// TODO A6 - Animation
 
-	 public void updateTransformations() {
+	public void updateTransformations() {
+
 		// Loop Through All The Timelines
 		// And Update Transformations Accordingly
 		// (You WILL Need To Use this.scene)
+		for (Iterator it = this.timelines.entrySet().iterator(); it.hasNext();) {
+			Map.Entry<String, AnimTimeline> pair = (Map.Entry<String, AnimTimeline>) it.next();
+			String a1k = pair.getKey();
+			AnimTimeline a1v = pair.getValue();
+			System.out.println(a1k + " = " + a1v);
 
-		// get pair of surrounding frames
-		// (function in AnimTimeline)
+			// get pair of surrounding frames
+			// (function in AnimTimeline)
+			int frame = 0;
+			AnimKeyframe[] outPair = new AnimKeyframe[2];
+			a1v.getSurroundingFrames(curFrame, outPair);
+			AnimKeyframe f1 = outPair[0];
+			AnimKeyframe f2 = outPair[1];
 
-		// get interpolation ratio
+			// get interpolation ratio
+			float ratio = (float) getRatio(f1.frame, f2.frame, curFrame);
+			float ratioMinus = (float) 1
+					- getRatio(f1.frame, f2.frame, curFrame);
+			// if min = max return 0
 
-		// interpolate translations linearly
+			Matrix4 xform1 = f1.transformation;
+			Matrix4 xform2 = f2.transformation;
 
-		// polar decompose axis matrices
+			// interpolate translations linearly
+			Vector3 xform1vec = new Vector3();
+			Vector3 xform2vec = new Vector3();
+			xform1.getTrans(xform1vec);
+			xform2.getTrans(xform2vec);
+			// translation with lerp in vector 3
+			xform1vec.lerp(xform2vec, ratio);
+			Matrix4 xformTrans = Matrix4.createTranslation(xform1vec);
 
-		// slerp rotation matrix and linearly interpolate scales
+			// polar decompose axis matrices
+			Matrix3 threeXform1 = new Matrix3(xform1);
+			Matrix3 threeXform2 = new Matrix3(xform2);
+			Matrix3 outR1 = new Matrix3();
+			Matrix3 outS1 = new Matrix3();
+			threeXform1.polar_decomp(outR1, outS1);
+//			System.out.println("outR1 = \n" + outR1 + "\noutS1 = \n" + outS1);
+			Matrix3 outR2 = new Matrix3();
+			Matrix3 outS2 = new Matrix3();
+			threeXform2.polar_decomp(outR2, outS2);
+//			System.out.println("outR2 = \n" + outR2 + "\noutS2 = \n" + outS2);
 
-		// combine interpolated R,S,and T
+			// slerp rotation matrix
+			Quat q1 = new Quat(outR1);
+			Quat q2 = new Quat(outR2);
+			Quat q3 = Quat.slerp(q1, q2, ratio);
 
+			// linearly interpolate scales
+			Matrix3 interpScale = new Matrix3();
+			interpScale.interpolate(outS1, outS2, ratio);
 
-	 }
+			// combine interpolated TRS
+			Matrix4 xformScale = new Matrix4(interpScale);
+			Matrix4 xformRot = new Matrix4();
+			q3.toRotationMatrix(xformRot);
+			
+			Matrix4 matrixTRS = xformTrans.clone().mulBefore(xformRot).clone().mulBefore(xformScale);
+			a1v.object.transformation.set(matrixTRS);
+			
+			
+			
+			// notification to scene that update has been made
+			this.scene.sendEvent(new SceneTransformationEvent(a1v.object));
+		}
+	}
 
-         public static float getRatio(int min, int max, int cur) {
-	     if(min == max) return 0f;
-	     float total = max - min;
-	     float diff = cur - min;
-	     return diff / total;
-	 }
+	public static float getRatio(int min, int max, int cur) {
+		if (min == max)
+			return 0f;
+		float total = max - min;
+		float diff = cur - min;
+		return diff / total;
+	}
 }
--- src/cs4620/ray2/surface/Box.java
+++ src/cs4620/ray2/surface/Box.java
@@ -1,13 +1,17 @@
 package cs4620.ray2.surface;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.lwjgl.BufferUtils;
 
 import cs4620.mesh.MeshData;
 import cs4620.ray2.IntersectionRecord;
 import cs4620.ray2.Ray;
+import edu.cornell.graphics.exr.ilmbaseto.Vector3;
 import egl.math.Vector3d;
+import egl.math.Vector4;
+import egl.math.Vector4d;
 
 /**
  * A class that represents an Axis-Aligned box. When the scene is built, the Box
@@ -68,13 +72,88 @@ public class Box extends Surface {
 	}
 
 	public void computeBoundingBox() {
-		// TODO#A7: Compute the bounding box and store the result in
+		// TODO#A7: NEEDS TESTING!! 
+		// Compute the bounding box and store the result in
 		// averagePosition, minBound, and maxBound.
 		// Hint: The bounding box is not the same as just minPt and maxPt,
 		// because
 		// this object can be transformed by a transformation matrix.
+		
+		
+		int numVert = 8;
+		
+		// Setup Base Case
+		double tempX = this.mesh.getPosition(0).x;
+		double tempY = this.mesh.getPosition(0).y;
+		double tempZ = this.mesh.getPosition(0).z;
+		// w is 1 for points
+		Vector4d temp4 = new Vector4d(tempX, tempY, tempZ, 1);
+		// Transform Vertices
+		this.tMat.mul(temp4);
+		
+		// Basic setup for minBound
+		double tempXmin = temp4.x;
+		double tempYmin = temp4.y;
+		double tempZmin = temp4.z;
+		// Basic setup for maxBound
+		double tempXmax = temp4.x;
+		double tempYmax = temp4.y;
+		double tempZmax = temp4.z;
+		// Basic setup for averagePosition
+		double tempXavg = temp4.x;
+		double tempYavg = temp4.y;
+		double tempZavg = temp4.z;
+		
+		// Loop through remaining cases
+		for (int i = 1; i < numVert; i++){
+			temp4.setZero();
 
+			tempX = this.mesh.getPosition(i).x;
+			tempY = this.mesh.getPosition(i).y;
+			tempZ = this.mesh.getPosition(i).z;
+			// w is 1 for points
+			temp4.set(tempX, tempY, tempZ, 1);
+			// Transform Vertices
+			this.tMat.mul(temp4);
+			
+			// if new x is less than minBound.x, store new minBound.x
+			if (temp4.x < tempXmin) {
+				tempXmin = temp4.x;
+			// else if new x is more than maxBound.x, store new maxBound.x
+			} else if (temp4.x > tempXmax) {
+				tempXmax = temp4.x;
+			}
+
+			// if new y is less than minBound.y, store new minBound.y
+			if (temp4.y < tempYmin) {
+				tempYmin = temp4.y;
+			// else if new y is more than maxBound.y, store new maxBound.y
+			} else if (temp4.y > tempYmax) {
+				tempYmax = temp4.y;
+			}
+
+			// if new z is less than minBound.z, store new minBound.z
+			if (temp4.z < tempZmin) {
+				tempZmin = temp4.z;
+			// else if new x is more than maxBound.x, store new maxBound.x
+			} else if (temp4.z > tempZmax) {
+				tempZmax = temp4.z;
+			}
+			
+			tempXavg = tempXavg + temp4.x;
+			tempYavg = tempYavg + temp4.y;
+			tempZavg = tempZavg + temp4.z;				
+		}
+				
+		// Set minBound and maxBound 
+		minBound.set(tempXmin, tempYmin, tempZmin);
+		maxBound.set(tempXmax, tempYmax, tempZmax);
         
+		// Set average position
+		tempXavg = tempXavg / numVert;
+		tempYavg = tempYavg / numVert;
+		tempZavg = tempZavg / numVert;
+		averagePosition.set(tempXavg, tempYavg, tempZavg);
 	}
 
 	public boolean intersect(IntersectionRecord outRecord, Ray ray) {
